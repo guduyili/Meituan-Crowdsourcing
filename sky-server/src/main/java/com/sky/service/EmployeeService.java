5d9c6b8c88b2541f8c17d708ecdd1506ae845ce6
Guduyili <LZY3376163189@163.com>
2024-11-18 20:57:44 +0800

    新增员工

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.sky.service;
 
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 
@@ -12,4 +13,10 @@ public interface EmployeeService {
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
+
+    /**
+     * 新增牛马
+     * @param employeeDTO
+     */
+    void save(EmployeeDTO employeeDTO);
 }
